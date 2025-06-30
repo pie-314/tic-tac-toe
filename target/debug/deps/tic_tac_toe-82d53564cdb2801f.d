ca4f04ea6ff5ff2b80dbe724fd50fa708190b8ad
Mr-Pi-a <aadarsh.official.xz@gmail.com>
2025-07-01 00:14:58 +0530

    decider mostly completed

--- target/debug/deps/tic_tac_toe-82d53564cdb2801f.d
+++
@@ -1,5 +0,0 @@
-/media/pi/NewVolume/other_disk/programming/tic-tac-toe/target/debug/deps/tic_tac_toe-82d53564cdb2801f: src/main.rs
-
-/media/pi/NewVolume/other_disk/programming/tic-tac-toe/target/debug/deps/tic_tac_toe-82d53564cdb2801f.d: src/main.rs
-
-src/main.rs:
--- target/debug/tic-tac-toe.d
+++
@@ -1,1 +0,0 @@
-/media/pi/NewVolume/other_disk/programming/tic-tac-toe/target/debug/tic-tac-toe: /media/pi/NewVolume/other_disk/programming/tic-tac-toe/src/main.rs
